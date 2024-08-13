a1ce4ae80d8b64707e19bbea9a2e1b685f1db05e
Joy1024 <412555203@qq.com>
2024-08-13 20:59:46 +0800

    Docker增加nginx启动

--- build/docker/entrypoint.sh
+++ build/docker/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
+systemctl start nginx
 
 export OK_STACK_HOME="${OK_STACK_DIR}"
-#sh $OK_STACK_HOME/bin/startup.sh
-
-# --chuid ${OK_STAR_USER}:${OK_STAR_USER} \
 exec start-stop-daemon --start --exec "$OK_STACK_HOME/bin/startup.sh"
